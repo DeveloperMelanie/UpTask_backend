796844b0a86c04811b47ad1e8951fc33591efd9a
Melanie <melanie16203@gmail.com>
2022-03-30 16:24:06 -0300

    nodemailer secure

--- helpers/emails.js
+++ helpers/emails.js
@@ -8,7 +8,9 @@ export const registrationEmail = async user => {
     const { name, email, token } = user
 
     const transport = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
@@ -29,7 +31,9 @@ export const forgotPasswordEmail = async user => {
     const { name, email, token } = user
 
     const transport = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
